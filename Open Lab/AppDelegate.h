2dda72ea4ac39171db3a55527dd38d6d95ade162
Jeffrey Brice <jbrice@jeffs-mac-pro.gateway.pace.com>
2013-11-05 22:17:57 -0800

    Initial Commit

---
+++ Open Lab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Open Lab
+//
+//  Created by Jeffrey Brice on 11/5/13.
+//  Copyright (c) 2013 Spacebooke Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Open Lab/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Open Lab
+//
+//  Created by Jeffrey Brice on 11/5/13.
+//  Copyright (c) 2013 Spacebooke Games. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
