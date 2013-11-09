a7bfb43ecda3268afc23c4634f158e93969dfba3
Jeffrey Brice <jbrice@jeffs-mac-pro.gateway.pace.com>
2013-11-09 07:26:45 -0800

    Added toolbar. Changed radio “Locked” buttons to buttons that also have room names. Clicking these displays a padlock showing Lab locked status. Added web links to about page.

--- Open Lab/AppDelegate.h
+++ Open Lab/AppDelegate.h
@@ -8,8 +8,70 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+    IBOutlet NSMenu *statusMenu;
+    NSStatusItem *statusItem;
+    NSImage * statusImage;
+    NSImage * statusHighlightImage;
+    IBOutlet NSImageView *lockImage;
+    IBOutlet NSImageView *unlockImage;
+    IBOutlet NSImageView *lockImage108;
+    IBOutlet NSImageView *unlockImage108;
+    IBOutlet NSImageView *grad109Locked;
+    IBOutlet NSImageView *grad117Locked;
+    IBOutlet NSImageView *grad118Locked;
+    IBOutlet NSImageView *undergrad116Locked;
+    IBOutlet NSImageView *ADRLocked;
+    IBOutlet NSImageView *foleyCTRLLocked;
+    IBOutlet NSImageView *foleyLiveLocked;
+    IBOutlet NSImageView *studioLocked;
+    IBOutlet NSImageView *studioBoothLocked;
+    IBOutlet NSImageView *vocalLocked;
+    IBOutlet NSImageView *practiceLocked;
+    IBOutlet NSImageView *practiceLLocked;
+    IBOutlet NSImageView *practiceRLocked;
+    IBOutlet NSImageView *grad109unLocked;
+    IBOutlet NSImageView *grad117unLocked;
+    IBOutlet NSImageView *grad118unLocked;
+    IBOutlet NSImageView *undergrad116unLocked;
+    IBOutlet NSImageView *ADRunLocked;
+    IBOutlet NSImageView *foleyCTRLunLocked;
+    IBOutlet NSImageView *foleyLiveunLocked;
+    IBOutlet NSImageView *studiounLocked;
+    IBOutlet NSImageView *studioBoothunLocked;
+    IBOutlet NSImageView *vocalunLocked;
+    IBOutlet NSImageView *practiceunLocked;
+    IBOutlet NSImageView *practiceLunLocked;
+    IBOutlet NSImageView *practiceRunLocked;
+}
+
 
 @property (assign) IBOutlet NSWindow *window;
+
+-(IBAction)lockMIDI:(id)sender;
+-(IBAction)lock108:(id)sender;
+-(IBAction)lock109:(id)sender;
+-(IBAction)lock116:(id)sender;
+-(IBAction)lock117:(id)sender;
+-(IBAction)lock118:(id)sender;
+-(IBAction)lockFoleyCTRL:(id)sender;
+-(IBAction)lockFoleyLive:(id)sender;
+-(IBAction)lockPractice:(id)sender;
+-(IBAction)lockPracticeL:(id)sender;
+-(IBAction)lockPracticeR:(id)sender;
+-(IBAction)lockStudio:(id)sender;
+-(IBAction)lockStudioBooth:(id)sender;
+-(IBAction)lockVocal:(id)sender;
+-(IBAction)lockADR:(id)sender;
+-(IBAction)goToGit:(id)sender;
+-(IBAction)goToJeff:(id)sender;
+-(IBAction)goToCoin:(id)sender;
+
+
+
+
+
+
+
 
 @end
--- Open Lab/AppDelegate.m
+++ Open Lab/AppDelegate.m
@@ -8,11 +8,255 @@
 
 #import "AppDelegate.h"
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    
+    BOOL undergrad108Locked;
+    BOOL graduate109Locked;
+    BOOL graduate117Locked;
+    BOOL graduate118Locked;
+    BOOL undergraduate116Locked;
+    BOOL MIDILocked;
+    BOOL ADRLabLocked;
+    BOOL foleyLabCTRLLocked;
+    BOOL foleyLabLiveLocked;
+    BOOL studio712Locked;
+    BOOL studio712BoothLocked;
+    BOOL vocalBoothLocked;
+    BOOL practiceRoomLocked;
+    BOOL practiceRoomLLocked;
+    BOOL practiceRoomRLocked;
+
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+}
+
+- (void)awakeFromNib {
+    
+    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    NSBundle *bundle = [NSBundle mainBundle];
+    
+    statusImage = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"person" ofType:@"png"]];
+    statusHighlightImage = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"person" ofType:@"png"]];
+    
+    [statusItem setImage:statusImage];
+    [statusItem setAlternateImage:statusHighlightImage];
+    [statusItem setMenu:statusMenu];
+    [statusItem setToolTip:@"SAY SOMETHING"];
+    [statusItem setHighlightMode:YES];
+    undergrad108Locked = NO;
+    graduate109Locked = NO;
+    graduate117Locked = NO;
+    graduate118Locked = NO;
+    undergraduate116Locked = NO;
+    MIDILocked = NO;
+    ADRLabLocked = NO;
+    foleyLabCTRLLocked = NO;
+    foleyLabLiveLocked = NO;
+    studio712Locked = NO;
+    studio712BoothLocked = NO;
+    vocalBoothLocked = NO;
+    practiceRoomLocked = NO;
+    practiceRoomLLocked = NO;
+    practiceRoomRLocked = NO;
+    
+}
+
+// Sent actions for Room lock buttons
+-(IBAction)lockMIDI:(id) sender  {
+    if (!MIDILocked) {
+        unlockImage.hidden = NO;
+        lockImage.hidden = YES;
+        MIDILocked = YES;
+    }
+    else {
+        unlockImage.hidden = YES;
+        lockImage.hidden = NO;
+        MIDILocked = NO;
+    }
+    
+}
+-(IBAction)lock108:(id) sender  {
+    if (!undergrad108Locked) {
+        unlockImage108.hidden = NO;
+        lockImage108.hidden = YES;
+        undergrad108Locked = YES;
+    }
+    else {
+        unlockImage108.hidden = YES;
+        lockImage108.hidden = NO;
+        undergrad108Locked = NO;
+    }
+    
+}
+-(IBAction)lock109:(id)sender {
+    if (!graduate109Locked) {
+        grad109unLocked.hidden = NO;
+        grad109Locked.hidden = YES;
+        graduate109Locked = YES;
+    }
+    else {
+        grad109unLocked.hidden = YES;
+        grad109Locked.hidden = NO;
+        graduate109Locked = NO;
+    }
+}
+-(IBAction)lock116:(id)sender {
+    if (!undergraduate116Locked) {
+        undergrad116unLocked.hidden = NO;
+        undergrad116Locked.hidden = YES;
+        undergraduate116Locked = YES;
+    }
+    else {
+        undergrad116unLocked.hidden = YES;
+        undergrad116Locked.hidden = NO;
+        undergraduate116Locked = NO;
+    }
+}
+-(IBAction)lock117:(id)sender {
+    if (!graduate117Locked) {
+        grad117unLocked.hidden = NO;
+        grad117Locked.hidden = YES;
+        graduate117Locked = YES;
+    }
+    else {
+        grad117unLocked.hidden = YES;
+        grad117Locked.hidden = NO;
+        graduate117Locked = NO;
+    }
+}
+-(IBAction)lock118:(id)sender {
+    if (!graduate118Locked) {
+        grad118unLocked.hidden = NO;
+        grad118Locked.hidden = YES;
+        graduate118Locked = YES;
+    }
+    else {
+        grad118unLocked.hidden = YES;
+        grad118Locked.hidden = NO;
+        graduate118Locked = NO;
+    }
+}
+-(IBAction)lockFoleyCTRL:(id)sender {
+    if (!foleyLabCTRLLocked) {
+        foleyCTRLunLocked.hidden = NO;
+        foleyCTRLLocked.hidden = YES;
+        foleyLabCTRLLocked = YES;
+    }
+    else {
+        foleyCTRLunLocked.hidden = YES;
+        foleyCTRLLocked.hidden = NO;
+        foleyLabCTRLLocked = NO;
+    }
+}
+-(IBAction)lockFoleyLive:(id)sender {
+    if (!foleyLabLiveLocked) {
+        foleyLiveunLocked.hidden = NO;
+        foleyLiveLocked.hidden = YES;
+        foleyLabLiveLocked = YES;
+    }
+    else {
+        foleyLiveunLocked.hidden = YES;
+        foleyLiveLocked.hidden = NO;
+        foleyLabLiveLocked = NO;
+    }
+}
+-(IBAction)lockPractice:(id)sender {
+    if (!practiceRoomLocked) {
+        practiceunLocked.hidden = NO;
+        practiceLocked.hidden = YES;
+        practiceRoomLocked = YES;
+    }
+    else {
+        practiceunLocked.hidden = YES;
+        practiceLocked.hidden = NO;
+        practiceRoomLocked = NO;
+    }
+}
+-(IBAction)lockPracticeL:(id)sender {
+    if (!practiceRoomLLocked) {
+        practiceLunLocked.hidden = NO;
+        practiceLLocked.hidden = YES;
+        practiceRoomLLocked = YES;
+    }
+    else {
+        practiceLunLocked.hidden = YES;
+        practiceLLocked.hidden = NO;
+        practiceRoomLLocked = NO;
+    }
+}
+-(IBAction)lockPracticeR:(id)sender {
+    if (!practiceRoomRLocked) {
+        practiceRunLocked.hidden = NO;
+        practiceRLocked.hidden = YES;
+        practiceRoomRLocked = YES;
+    }
+    else {
+        practiceRunLocked.hidden = YES;
+        practiceRLocked.hidden = NO;
+        practiceRoomRLocked = NO;
+    }
+}
+-(IBAction)lockStudio:(id)sender {
+    if (!studio712Locked) {
+        studiounLocked.hidden = NO;
+        studioLocked.hidden = YES;
+        studio712Locked = YES;
+    }
+    else {
+        studiounLocked.hidden = YES;
+        studioLocked.hidden = NO;
+        studio712Locked = NO;
+    }
+}
+-(IBAction)lockStudioBooth:(id)sender {
+    if (!studio712BoothLocked) {
+        studioBoothunLocked.hidden = NO;
+        studioBoothLocked.hidden = YES;
+        studio712BoothLocked = YES;
+    }
+    else {
+        studioBoothunLocked.hidden = YES;
+        studioBoothLocked.hidden = NO;
+        studio712BoothLocked = NO;
+    }
+}
+-(IBAction)lockVocal:(id)sender {
+    if (!vocalBoothLocked) {
+        vocalunLocked.hidden = NO;
+        vocalLocked.hidden = YES;
+        vocalBoothLocked = YES;
+    }
+    else {
+        vocalunLocked.hidden = YES;
+        vocalLocked.hidden = NO;
+        vocalBoothLocked = NO;
+    }
+}
+-(IBAction)lockADR:(id)sender {
+    if (!ADRLabLocked) {
+        ADRunLocked.hidden = NO;
+        ADRLocked.hidden = YES;
+        ADRLabLocked = YES;
+    }
+    else {
+        ADRunLocked.hidden = YES;
+        ADRLocked.hidden = NO;
+        ADRLabLocked = NO;
+    }
+}
+
+// Sent actions for weblinks
+-(IBAction)goToGit:(id)sender {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: @"http://github.com/pumodi"]];
+}
+-(IBAction)goToJeff:(id)sender {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: @"http://jeffbricemusic.com"]];
+}
+-(IBAction)goToCoin:(id)sender {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString: @"http://probendingmaster.com/bitcoin.html"]];
 }
 
 @end
